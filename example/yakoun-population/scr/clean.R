c1b796c446dec8be98c245d6ba219e207593d654
Ayla Pearson <ayla@poissonconsulting.ca>
2022-07-05 10:47:05 -0700

    adding example analysis folder

---
+++ example/yakoun-population/scr/clean.R
@@ -0,0 +1,6 @@
+# Clean Data
+
+visit <- readRDS("example/yakoun-population/output/read/visit.RDS")
+capture <- readRDS("example/yakoun-population/output/read/capture.RDS")
+
+visit
---
+++ example/yakoun-population/scr/read.R
@@ -0,0 +1,20 @@
+# Read Data
+
+#### Visit ####
+
+visit <- read.csv("example/yakoun-population/data/visit.csv")
+
+saveRDS(
+  visit,
+  "example/yakoun-population/output/read/visit.RDS"
+)
+
+#### Capture ####
+
+capture <- read.csv("example/yakoun-population/data/captures.csv")
+
+saveRDS(
+  visit,
+  "example/yakoun-population/output/read/capture.RDS"
+)
+
