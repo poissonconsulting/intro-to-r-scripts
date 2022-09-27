a8d431fbed31943e9fc43538bede7df75abf0652
Ayla Pearson <ayla@poissonconsulting.ca>
2022-09-27 15:28:54 -0700

    Adding new examples

--- 04-data-types.R
+++ 04-data-types.R
@@ -38,3 +38,79 @@ x <- as.integer(x)
 typeof(x)
 
 as.integer(c("1", "one", "hippo", "4.2"))
+
+# Double or integer?
+typeof(2)
+typeof(2.0)
+typeof(2.1)
+typeof(2L)
+typeof(2.0L)
+typeof(100L)
+typeof(as.integer(3.2))
+
+# What makes these all type character? What do they all have in common?
+typeof("")
+typeof("a")
+typeof("The")
+typeof("!")
+typeof("Fish are friends!")
+typeof("example@gmail.com")
+
+# Are they logical or character? Why?
+typeof(TRUE)
+typeof("TRUE")
+typeof(FALSE)
+typeof("FALSE")
+typeof(T)
+typeof("F")
+
+# What does the `cat()` function do?
+word_first <- "This"
+word_second <- "adds"
+word_third <- "words together."
+cat(word_first, word_second, word_third)
+
+
+# Practice making difference sequences, what happens as you vary the values input into `seq()`?
+seq(1, 10, 1)
+seq(1, 10, 2)
+
+seq(1, 5, 1)
+seq(1, 5, 2)
+
+seq(0, 10, 1)
+seq(0, 10, 2)
+
+# Can you create a sequence from zero to 21 by every three numbers to give the sequence 0, 3, 6, 9, 12, 15, 18, 21?
+
+
+# Can all values be converted to another type successfully?
+# What happens to the word "Dog" when forced to be an integer?
+
+value_to_test <- TRUE
+typeof(value_to_test)
+as.integer(value_to_test)
+as.numeric(value_to_test)
+as.character(value_to_test)
+as.logical(value_to_test)
+
+value_to_test <- "Dog"
+typeof(value_to_test)
+as.integer(value_to_test)
+as.numeric(value_to_test)
+as.character(value_to_test)
+as.logical(value_to_test)
+
+value_to_test <- 0
+typeof(value_to_test)
+as.integer(value_to_test)
+as.numeric(value_to_test)
+as.character(value_to_test)
+as.logical(value_to_test)
+
+value_to_test <- 2.5
+typeof(value_to_test)
+as.integer(value_to_test)
+as.numeric(value_to_test)
+as.character(value_to_test)
+as.logical(value_to_test)
