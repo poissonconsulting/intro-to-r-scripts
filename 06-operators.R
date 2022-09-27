dc54b6a16d31c577d4b9f3dec3fbfb1b677f0166
Ayla Pearson <ayla@poissonconsulting.ca>
2022-09-27 15:31:22 -0700

    adding more examples

--- 06-operators.R
+++ 06-operators.R
@@ -4,9 +4,17 @@ c("this will not save")
 # x is saved under Values
 x <- c("this will save")
 
+
 print(x)
 x <- c("Now it has been overwritten")
 print(x)
+
+# lower case species
+plant_parts <- c("shoot", "root", "leaf", "stem")
+
+ print(Plant_Parts)
+ print(plant_Parts)
+ print(plant_parts)
 
 # How many decimal places does R display?
 2.333333334353463984821113
@@ -88,3 +96,156 @@ c(TRUE, TRUE, FALSE) & c(FALSE, TRUE, FALSE)
 
 # Or (vectorized/element wise)
 c(TRUE, TRUE) | c(FALSE, TRUE)
+
+vals <- c(2.1, 4.3, 5.3, 7.1, 9.4)
+
+log(vals)
+exp(vals)
+mean(vals)
+sd(vals)
+
+# What happens to the variable `first_name`? What value does it have in the start and the end?
+first_name <- "Ayla"
+first_name
+last_name <- "Pearson"
+last_name
+cat(first_name, last_name)
+
+first_name <- "Kathy"
+first_name
+
+cat(first_name, last_name)
+
+# What is the answer to each equation?
+
+(2 + 6) - 8
+
+14 * 1
+
+50 * 1/2
+
+40 %% 2
+
+50 %/% 1
+
+50 %/% 2
+
+50 %/% 5
+
+10^2 / 2
+
+(7 * 2) - (60^1 / 4)
+
+20 + 2 / 3 * 6^3 - 22 + 18 * 6
+
+20 + (2 / 3 * 6^3) - (22 + 18) * 6
+
+# True or False?
+
+w <- 20
+x <- 100
+y <- 20
+z <- 125
+
+z < y
+
+y < x
+
+w == y
+
+z == x
+
+z != w
+
+z != z
+
+x <= z
+
+y >= w
+
+"no" == "yes"
+
+"site_a" == "site_A"
+
+"lake" == "lake"
+
+"abc" != "abd"
+
+# True or False?
+x <- c(5, 10, 13, 15, 16, 17, 12, 40, 40, 2)
+x
+
+result_a <- 4 %in% x
+result_a
+result_b <- 5 %in% x
+result_b
+result_c <- 40 %in% x
+result_c
+
+!result_a
+!result_b
+!result_c
+
+!!result_a
+
+# True or False?
+
+100 > 101 && 1 < 5
+
+1 == 1 && 2 < 3
+
+"blue" == "blue" && 4 == 4
+
+"blue" == "brown" && 4 == 4
+
+10 <= 11 && 14 > 10
+
+7 < 5 && 10 > 20
+
+# True or False?
+
+100 > 101 || 1 < 5
+
+1 == 1 || 2 < 3
+
+"blue" == "blue" || 4 == 4
+
+"blue" == "brown" || 4 == 4
+
+10 <= 11 || 14 > 10
+
+7 < 5 || 10 > 20
+
+# What is the result?
+log(1)
+
+log(0)
+
+log(100)
+
+log(2.7)
+
+exp(0)
+
+exp(1)
+
+exp(100)
+
+exp(1000)
+
+measured_value <- c(1, 1, 3, 5, 7, 3, 5, 4)
+measured_value
+mean(measured_value)
+sd(measured_value)
+median(measured_value)
+
+
+measured_value_2 <- c(1, 1, 3, 5, 7, 3, 5, 4, NA)
+measured_value_2
+mean(measured_value_2)
+sd(measured_value_2)
+median(measured_value_2)
+# Try adding the `na.rm = TRUE` and see what happens.
+mean(measured_value_2, na.rm = TRUE)
+sd(measured_value_2, na.rm = TRUE)
+median(measured_value_2, na.rm = TRUE)
